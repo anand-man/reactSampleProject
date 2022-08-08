1f53d61c42403cb1918aa4952b4e77ae91b78515
Anand <mksaarya@gmail.com>
2022-08-08 11:52:18 +0530

    images issue fixed

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React, { lazy, Suspense } from "react";
 import "./styles/styles.scss";
 import {Routes, Route} from "react-router-dom";
 import Loader from "./components/commons/Loader";
+import ProductKart from "./components/Kart/ProductKart";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import OrderPlaced from "./components/OrderPlaced/OrderPlaced";
+import CheckoutHandler from "./components/Checkout/CheckoutHandler";
 
-const Header = lazy(() => import("./components/Header/Header"));
-const Footer = lazy(() => import("./components/Footer/Footer"));
 const HomePage = lazy(() => import("./components/HomePage/HomePage"));
 const SecondLevelBanner = lazy(() => import("./components/SecondLevelBanner/SecondLevelBanner"));
 const Products = lazy(() => import("./components/Product/Product"));
 const ProductDetails = lazy(() => import("./components/Product/ProductDetails"));
-const ProductKart = lazy(() => import("./components/Kart/ProductKart"));
-const CheckoutHandler = lazy(() => import("./components/Checkout/CheckoutHandler"));
-const OrderPlaced = lazy(() => import("./components/OrderPlaced/OrderPlaced"));
 
 function App() {
   return (
